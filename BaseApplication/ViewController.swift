d1768fa750720c7adbc345e3d34229578173c08f
anh.mai <anh.mai@eastgate-software.com>
2019-03-19 23:21:20 +0700

    Initial Commit

---
+++ BaseApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseApplication
+//
+//  Created by Hoang Anh on 3/19/19.
+//  Copyright © 2019 Hoang Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
